6438b04acd42f1dc52593dc115f5e758a22d6e7d
Victor Pontes <victorpontes2013@gmail.com>
2024-06-29 14:47:01 -0700

    api setup on login_service.go

--- lambda/login_service/login_service.go
+++ lambda/login_service/login_service.go
@@ -3,16 +3,23 @@ package main
 import (
 	"context"
 
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/events"
+
 	"github.com/aws/aws-lambda-go/lambda"
 )
+
+type Request = events.APIGatewayProxyRequest
+type Response = events.APIGatewayProxyResponse
 
 type Event struct {
 }
 
-func handler(ctx context.Context, event *Event) (*string, error) {
-	print("Hello World")
-	message := "Hello World"
-	return &message, nil
+func handler(ctx context.Context, request Request) (Response, error) {
+	fmt.Printf(request.Body)
+	response := Response{StatusCode: 200, Body: "Hello World"}
+	return response, nil
 }
 
 func main() {

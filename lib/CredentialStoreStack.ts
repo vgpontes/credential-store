c737052cd53fcbb944f7e6481fcb8aefe1f8de46
Victor Pontes <victorpontes2013@gmail.com>
2024-06-28 17:30:46 -0700

    add rds database

--- lib/CredentialStoreStack.ts
+++ lib/CredentialStoreStack.ts
@@ -1,13 +1,19 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { LoginService } from './LoginService/LoginService';
+import { Vpc } from 'aws-cdk-lib/aws-ec2';
 
 export class CredentialStoreStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id);
 
+    const credentialStoreVpc = new Vpc(this, 'CredentialStoreVPC', {
+      vpcName: 'credential-store-vpc',
+    });
+
     new LoginService(this, 'CredentialStoreLoginService', {
-        appName: 'credential-store'
+        appName: 'credential-store',
+        vpc: credentialStoreVpc
     });
   }
 }

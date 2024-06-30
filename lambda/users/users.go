0a7765ef64785e74f5de7a20715e39c7a1770abc
motohaton <99621905+motohaton@users.noreply.github.com>
2024-06-30 16:09:26 -0700

    /users GET methods

--- lambda/users/users.go
+++ lambda/users/users.go
@@ -1,27 +1,62 @@
 package main
 
 import (
-	"context"
-
-	"fmt"
-
-	"github.com/aws/aws-lambda-go/events"
+	"encoding/json"
+	"net/http"
 
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/awslabs/aws-lambda-go-api-proxy/httpadapter"
 )
 
-type Request = events.APIGatewayProxyRequest
-type Response = events.APIGatewayProxyResponse
-
-type Event struct {
+type User struct {
+	UserID   uint   `json:"userID"`
+	Username string `json:"userName"`
+	Password string `json:"password"`
+	Salt     string `json:"salt"`
+	IsAdmin  bool   `json:"isAdmin"`
 }
 
-func handler(ctx context.Context, request Request) (Response, error) {
-	fmt.Printf(request.Body)
-	response := Response{StatusCode: 200, Body: "Hello World"}
-	return response, nil
+var Users = []User{
+	{UserID: 12345, Username: "makarimi", Password: "h2Hax.", Salt: "w3dfwdf", IsAdmin: false},
+	{UserID: 12346, Username: "vpontes", Password: "d32r2r3", Salt: "qdwqwfwefwf", IsAdmin: true},
+	{UserID: 12347, Username: "sakulka", Password: "dw3edwefwe", Salt: "edwedfw", IsAdmin: false},
+}
+
+func handleGetUser(w http.ResponseWriter, r *http.Request) {
+	userName := r.PathValue("username")
+	for _, user := range Users {
+		if user.Username == userName {
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusOK)
+			json.NewEncoder(w).Encode(user)
+		} else {
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusBadRequest)
+		}
+	}
+}
+
+func handleGetUsers(w http.ResponseWriter, r *http.Request) {
+	var user User
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+	}
+	if user.IsAdmin {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(Users)
+	} else {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+	}
 }
 
 func main() {
-	lambda.Start(handler)
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /users/{username}", handleGetUser)
+	mux.HandleFunc("GET /users", handleGetUsers)
+	lambda.Start(httpadapter.NewV2(mux).ProxyWithContext)
 }

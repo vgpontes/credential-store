1fcdf70119f41e13904188086358fe572113a2e3
motohaton <99621905+motohaton@users.noreply.github.com>
2024-06-30 10:41:49 -0700

    api-gateway test using httpadapter

--- lambda/auth_service/auth_service.go
+++ lambda/auth_service/auth_service.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"context"
-
-	"fmt"
+	"encoding/json"
+	"net/http"
 
 	"github.com/aws/aws-lambda-go/events"
+	"github.com/awslabs/aws-lambda-go-api-proxy/httpadapter"
 
 	"github.com/aws/aws-lambda-go/lambda"
 )
@@ -16,12 +16,35 @@ type Response = events.APIGatewayProxyResponse
 type Event struct {
 }
 
-func handler(ctx context.Context, request Request) (Response, error) {
+type User struct {
+	UserID   uint   `json:"userID"`
+	Username string `json:"userName"`
+	Password string `json:"password"`
+	Salt     string `json:"salt"`
+}
+
+func newUser(userName string, password string) *User {
+	return &User{
+		UserID:   12345,
+		Username: userName,
+		Password: password,
+		Salt:     "fewdnwqeelrdn",
+	}
+}
+
+/*func handler(ctx context.Context, request Request) (Response, error) {
 	fmt.Printf(request.Body)
 	response := Response{StatusCode: 200, Body: "Hello World"}
 	return response, nil
-}
+} */
 
 func main() {
-	lambda.Start(handler)
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /login", func(w http.ResponseWriter, r *http.Request) {
+		user := newUser("makarimi", "erhjdeer145")
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusCreated)
+		json.NewEncoder(w).Encode(user)
+	})
+	lambda.Start(httpadapter.New(mux).ProxyWithContext)
 }
--- lambda/go.mod
+++ lambda/go.mod
@@ -4,4 +4,7 @@ go 1.22.3
 
 require github.com/aws/aws-lambda-go v1.47.0
 
-require github.com/stretchr/testify v1.9.0 // indirect
+require (
+	github.com/awslabs/aws-lambda-go-api-proxy v0.16.2 // indirect
+	github.com/stretchr/testify v1.9.0 // indirect
+)

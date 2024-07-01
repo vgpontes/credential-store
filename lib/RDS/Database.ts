496aefa3856beb02bf7ce7bcd66c7e2bb7be3336
Victor Pontes <victorpontes2013@gmail.com>
2024-07-01 08:37:29 -0700

    fix error in database infra

--- lib/RDS/Database.ts
+++ lib/RDS/Database.ts
@@ -1,6 +1,7 @@
 import { InstanceClass, InstanceSize, InstanceType, Vpc } from "aws-cdk-lib/aws-ec2";
-import { DatabaseInstance, DatabaseInstanceEngine } from "aws-cdk-lib/aws-rds";
-import { ISecret } from "aws-cdk-lib/aws-secretsmanager";
+import { Key } from "aws-cdk-lib/aws-kms";
+import { Credentials, DatabaseInstance, DatabaseInstanceEngine } from "aws-cdk-lib/aws-rds";
+import { ISecret, Secret } from "aws-cdk-lib/aws-secretsmanager";
 import { Construct } from "constructs";
 
 export class CredentialStoreDB extends Construct {
@@ -19,12 +20,16 @@ export class CredentialStoreDB extends Construct {
       instanceIdentifier: `credentialstoredb`,
       engine: DatabaseInstanceEngine.POSTGRES,
       vpc: credentialStoreVpc,
+      credentials: Credentials.fromGeneratedSecret('postgres', {
+        secretName: 'credential-store-db-credentials',
+        
+      }),
       allocatedStorage: 20, // GiB
       cloudwatchLogsRetention: 14,
       instanceType: InstanceType.of(InstanceClass.T3, InstanceSize.MICRO),
       publiclyAccessible: true
     });
 
-    this.dbInfo = database.secret?
+    this.dbInfo = database.secret!;
   }
 }

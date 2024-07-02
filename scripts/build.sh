6d87505cef56ca486928a648e70b95d813f78645
motohaton <99621905+motohaton@users.noreply.github.com>
2024-07-02 15:12:57 -0700

    db connection test fix2

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,5 +3,5 @@
 cd lambda
 go env -w GOOS=linux GOARCH=arm64
 go build -tags lambda.norpc -o ../build/auth_service/bootstrap ./auth_service/auth_service.go
-go build -tags lambda.norpc -o ../build/users/bootstrap ./users/users.go
+go build -tags lambda.norpc -o ../build/users/bootstrap ./users/users.go ./users/usersAPI.go ./users/usersDB.go ./users/usersTypes.go
 cd ..
